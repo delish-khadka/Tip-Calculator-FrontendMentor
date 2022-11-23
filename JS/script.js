79d9ae11ac59c943424fffb97a7edff5be993cb6
Delish Khadka <71542850+delish-khadka@users.noreply.github.com>
2022-11-24 00:03:36 +0545

    added tip division code

---
+++ JS/script.js
@@ -0,0 +1,54 @@
+const billAmount = document.querySelector("#bill-input");
+const numberOfPeople = document.querySelector("#number-of-people");
+const customTipPercentage = document.querySelector("#tip-input");
+const billTipAmount = document.querySelector("#tip-amount");
+const billTotalPerPerson = document.querySelector("#total-amount");
+const resetButton = document.querySelector("reset-button");
+const buttons = document.querySelectorAll("#tip-buttons");
+
+//Calculate Tip When Click On Tip Percentage Button
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    let tipvalue = e.target.innerText;
+    tipvalue = tipvalue.substr(0, tipvalue.length - 1);
+
+    if (billAmount.value === "") return;
+    if (numberOfPeople.value === "") numberOfPeople.value = 1;
+
+    calculateTip(parseFloat(billAmount.value), parseInt(tipvalue), parseInt(numberOfPeople.value));
+  });
+});
+
+//Calculate Tip When User Give Custom Tip Percentage Input
+customTipPercentage.addEventListener("blur", (e) => {
+  if (billAmount.value === "") {
+    resetEverything();
+    return;
+  }
+  if (numberOfPeople.value === "") numberOfPeople.value = 1;
+
+  calculateTip(parseFloat(billAmount.value), parseFloat(e.target.value), parseInt(numberOfPeople.value));
+});
+
+//Calculate Tip
+function calculateTip(billAmount, tipPercentage, numberOfPeople) {
+  let tipAmount = (billAmount * (tipPercentage / 100)) / numberOfPeople;
+  let tip = Math.floor(tipAmount * 100) / 100;
+  tip = tip.toFixed(2);
+
+  let totalAmount = (tipAmount * numberOfPeople + billAmount) / numberOfPeople;
+  totalAmount = totalAmount.toFixed(2);
+
+  billTipAmount.innerHTML = `$${tip}`;
+  billTotalPerPerson.innerHTML = `$${totalAmount}`;
+}
+
+//Reset Everything
+resetButton.addEventListener("click", resetEverything);
+function resetEverything() {
+  billTipAmount.innerHTML = "$0.00";
+  billTotalPerPerson.innerHTML = "$0.00";
+  billAmount.value = "";
+  numberOfPeople.value = "";
+  customTipPercentage.value = "";
+}
