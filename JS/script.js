e88898ad0a65496d113764e0de1b3838887ddda5
Delish Khadka <71542850+delish-khadka@users.noreply.github.com>
2022-11-24 20:31:28 +0545

    completed javascript functionality

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,13 @@
 const billAmount = document.querySelector("#bill-input");
 const numberOfPeople = document.querySelector("#number-of-people");
-const customTipPercentage = document.querySelector("#tip-input");
+const customTipPercentage = document.querySelector(".tip-input");
 const billTipAmount = document.querySelector("#tip-amount");
 const billTotalPerPerson = document.querySelector("#total-amount");
-const resetButton = document.querySelector("reset-button");
+const resetButton = document.querySelector("#reset-button");
 const buttons = document.querySelectorAll("#tip-buttons");
+
+// alert("yoo");
+// console.log(customTipPercentage);
 
 //Calculate Tip When Click On Tip Percentage Button
 buttons.forEach((button) => {
@@ -20,11 +23,12 @@ buttons.forEach((button) => {
 });
 
 //Calculate Tip When User Give Custom Tip Percentage Input
-customTipPercentage.addEventListener("blur", (e) => {
-  if (billAmount.value === "") {
+customTipPercentage.addEventListener("input", (e) => {
+  if (billAmount.value === "" || customTipPercentage.value === "") {
     resetEverything();
     return;
   }
+
   if (numberOfPeople.value === "") numberOfPeople.value = 1;
 
   calculateTip(parseFloat(billAmount.value), parseFloat(e.target.value), parseInt(numberOfPeople.value));
